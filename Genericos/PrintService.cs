4914b0bdc9a1ede6d825b5d451ed671be45b0d41
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-08-23 21:32:52 -0300

    Classe Generics

--- Genericos/PrintService.cs
+++ Genericos/PrintService.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace Genericos
 {
-    class PrintService
+    class PrintService<T>
     {
+        private T[] _value = new T [10];
+        private int _count = 0;
+
+        public void AddValue(T element)
+        {   
+            if(_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+
+            _value[_count] = element;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            return _value[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for(int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_value[i]+ ", ");
+            }
+            
+            if(_count > 0)
+            {
+                Console.Write(_count - 1);
+            }
+            Console.Write("]");
+        }
     }
 }
