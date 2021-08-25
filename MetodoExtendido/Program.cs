2eb57254e2f6b5b57618f65cc8c12bc5df9807ae
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-08-25 09:38:47 -0300

    extension methods

---
+++ MetodoExtendido/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MetodoExtendido.Extensions;
+
+namespace MetodoExtendido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            DateTime dt = new DateTime(2021, 08, 25, 7, 45, 21);
+            Console.WriteLine(dt.ElapsedTime());
+            */
+
+            string s1 = "Good morning dear student";
+            Console.WriteLine(s1.Cut(10));
+
+        }
+    }
+}
